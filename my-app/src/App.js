6e29ee5a7b8db256f57491176e9c81ff217c6be3
leilasalemm <leila.salem96@gmail.com>
2024-03-09 21:48:28 -0500

    new log in and home title love u guys

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import backgroundImage from './THRFT.jpg';
 import './App.css';
 import LoginPage from './LogIn.js'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import  { useNavigate } from 'react-router-dom'
-// test
+import { createBrowserRouter, RouterProvider, Link, useNavigate } from 'react-router-dom';
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello world!<button onClick={() => {
-       // Redirect to the dashboard after login
-    }}>Hello</button></div>,
+    path: '/',
+    element: (
+      <div>
+        Hello world!
+        <Link to="/login">
+          <button>Log in</button>
+        </Link>
+      </div>
+    ),
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
   },
 ]);
